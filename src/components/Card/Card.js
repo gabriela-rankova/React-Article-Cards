808ed0b2411a8d2f4f065b0764896d26f229efca
Gabriela Rankova <grankova@a1.bg>
2021-12-05 20:43:52 +0200

    card - add category

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -12,6 +12,7 @@ const Card = ({ record }) => {
       <div className="card-content">
         <h1 className="card-article-title">{record.title}</h1>
         <div className="card-article-content">{record.content}</div>
+        <div className="card-article-category">{record.category}</div>
       </div>
     </div>
   );
